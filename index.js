0159051d797821a04a66374d14a591159b20717e
rasmus <rasmus.linde@voco.ee>
2025-10-16 10:51:59 +0300

    javatund1

---
+++ index.js
@@ -0,0 +1,104 @@
+// console.log("Hello");
+// console.log("I like dogs!");
+
+// window.alert("Alert");
+// window.alert("Dogs are cool");
+
+// -------
+
+// document.getElementById("myH1").textContent = "Hello";
+// document.getElementById("myP").textContent = "Dogs are cool!";
+
+// ---------
+
+// let x;
+// x = 99;
+
+// console.log(x);
+
+// let age = 25;
+// console.log(age);
+
+// console.log(`Sa oled ${age} aastat vana`);
+
+// let fullName = "Rasmus Linde";
+// let age = 28;
+// let isStudent = true;
+
+// document.getElementById("p1").textContent = `Your name is ${fullName}`;
+// document.getElementById("p2").textContent = `You are ${age} years old`;
+// document.getElementById("p3").textContent = `Enrolled: ${isStudent}`;
+
+// let students = 30;
+
+// students = students + 1;
+// students = students - 1;
+// students = students * 2;
+// students = students / 2;
+// students = students ** 2;
+// students = students % 2;
+
+// students += 1;
+// students -= 1;
+// students *= 2;
+
+// students++;
+// students--;
+
+// let result = 1 + 2 * 3 + 4 ** 2;
+
+// let result = (12 % 5) + 8 / 2;
+
+// let result = 6 / 2 ** (2+5);
+
+// console.log(result);
+
+// 1. window prompt
+
+// let username = window.prompt("whats your username?");
+
+// console.log(username);
+
+// 2. html box
+
+// let username;
+
+// document.getElementById("mySubmit").onclick = function () {
+//   username = document.getElementById("myText").value;
+//   document.getElementById("myH1").textContent = `Hello ${username}`;
+// };
+
+// let age = window.prompt("How old are you?");
+// age = Number(age);
+// age += 1;
+
+// console.log(age, typeof age);
+
+// let x = "pizza";
+// let y = "pizza";
+// let z = "pizza";
+
+// x = Number(x);
+// y = String(y);
+// z = Boolean(z);
+
+// console.log(x, typeof x);
+// console.log(y, typeof y);
+// console.log(z, typeof z);
+
+// const = a variable that can't be changed
+
+// const PI = 3.14159;
+// let radius;
+// let circumference;
+
+// radius = Number(radius);
+
+// circumference = 2 * PI * radius;
+
+// document.getElementById("mySubmit").onclick = function () {
+//   radius = document.getElementById("myText").value;
+//   radius = Number(radius);
+//   circumference = 2 * PI * radius;
+//   document.getElementById("myH3").textContent = circumference + "cm";
+// };
